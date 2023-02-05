1a05ab3fcdeb92e63bf8c49f7d28a81a66ff6c60
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2023-02-05 11:10:01 -0500

    Created plans for game & list of possible answers

---
+++ hangman.py
@@ -0,0 +1,56 @@
+"""
+Simple hangman game
+"""
+
+# Set possible answers
+possible_answers = ["The Gay Agenda", "tacos", "Nyan Cat", \
+  "Numa Numa Yay", "Badgers and Mushrooms"]
+print(possible_answers)
+
+# Set num guesses allowed per round
+
+# Randomly select an answer for this round
+
+# Print un-guessed game screen - try to make it less grim (flower?)
+
+# Ask user to guess a letter, enter the answer, give up, or quit
+
+# Validate input
+
+# Ask user for valid input if not valid
+
+# Send user to answer guessing prompt if selected
+
+# Send user to char guessing prompt if selected
+
+# Reveal answer if user gave up
+
+# Exit if user quit
+
+# Handle correct char guess
+
+# Handle incorrect char guess
+
+# Handle correct answer guess
+
+# Handle incorrect answer guess
+
+# Create win screen
+
+# Create lost screen
+
+# Ask if user wants to play again or quit
+
+# Start new game in a way that allows code reuse if selected
+
+# Exit if selected
+
+# Optional later - add colors
+
+# Optional later - add different images to subtract from
+
+# Optional later - add option to import possible answer lists
+
+# Optional later - add option to select difficulty
+
+# Optional later - add ability to view guesses so far
