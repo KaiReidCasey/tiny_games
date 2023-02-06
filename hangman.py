b820408747d0824c0636832545d6c224d12b7eb9
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2023-02-06 11:08:33 -0500

    Hangman: Added print functions for all characters
    TODO: Add gameplay that selects them

--- hangman.py
+++ hangman.py
@@ -10,15 +10,98 @@ import random
 /(   )\
   U U
  """
-def print_default_character():
+def print_character_default():
 	print("  ^ ^\n (^_^)\n/(   )\\\n  U U")
+
+"""
+Prints:
+  ^ ^
+ (-_-)
+/(   )\
+  U U
+"""
+def print_character_miss_1():
+	print("  ^ ^\n (-_-)\n/(   )\\\n  U U")
+
+"""
+Prints:
+  ^ ^
+ (-_-)
+-(   )-
+  U U
+"""
+def print_character_miss_2():
+	print("  ^ ^\n (-_-)\n-(   )-\n  U U")
+
+"""
+Prints:
+  ^ ^
+ (u_u)
+ (^ ^)
+  U U
+"""
+def print_character_miss_3():
+	print("  ^ ^\n (u_u)\n (^ ^)\n  U U")
+
+"""
+Prints:
+  ^ ^
+ (u_u)
+ (^ ^)
+  o o
+"""
+def print_character_miss_4():
+	print("  ^ ^\n (u_u)\n (^ ^)\n  o o")
+
+"""
+Prints:
+  ^ ^
+ (o_o)
+ (^ ^)
+  o o
+"""
+def print_character_miss_5():
+	print("  ^ ^\n (o_o)\n (^ ^)\n  o o")
+
+"""Prints:
+  ^ ^
+ (^.^)
+\(   )/
+  U U
+  """
+def print_character_win():
+	print("  ^ ^\n (^.^)\n\\(   )/\n  U U")
+
+"""
+Prints:
+  Y Y
+ (XOX)
+\(   )/
+  o o
+"""
+def print_character_lose():
+	print("  Y Y\n (XOX)\n\\(   )/\n  o o")
 
 def print_character(num_round_guesses):
 	match num_round_guesses:
 		case 0:
-			print_default_character()
+			print_character_default()
+		case 1:
+			print_character_miss_1()
+		case 2:
+			print_character_miss_2()
+		case 3:
+			print_character_miss_3()
+		case 4:
+			print_character_miss_4()
+		case 5:
+			print_character_miss_5()
+		case 6:
+			print_character_lose()
+		case -1:
+			print_character_win()
 		case _:
-			print_default_character()
+			print_character_default()
 	pass
 
 def print_options():
@@ -35,6 +118,7 @@ POSSIBLE_ANSWERS = ["The Gay Agenda", "tacos", "Nyan Cat", \
 print(POSSIBLE_ANSWERS)
 
 # Set num guesses allowed per round
+# Can't be used in a case statement without dot notation
 MAX_GUESSES_PER_ROUND = 7
 
 # Randomly select an answer for this round
@@ -44,57 +128,15 @@ print(round_answer)
 num_guesses_this_round = 0
 
 # Print un-guessed game screen
-print_game_screen(num_guesses_this_round)
-"""
-1.
-  ^ ^
- (^_^)
-/(   )\
-  U U
-
-2.
-  ^ ^
- (-_-)
-/(   )\
-  U U
-
-3.
-  ^ ^
- (-_-)
--(   )-
-  U U
-
-4.
-  ^ ^
- (u_u)
- (^ ^)
-  U U
-
-5.
-  ^ ^
- (u_u)
- (^ ^)
-  o o
-
-6.
-  ^ ^
- (o_o)
- (^ ^)
-  o o
-
-7.
-  Y Y
- (XOX)
-\(   )/
-  o o
-
-Win.
-  ^ ^
- (^.^)
-\(   )/
-  U U
-
-"""
+# print_game_screen(num_guesses_this_round)
+print_game_screen(-1)
+print_game_screen(0)
+print_game_screen(1)
+print_game_screen(2)
+print_game_screen(3)
+print_game_screen(4)
+print_game_screen(5)
+print_game_screen(6)
 
 # Ask user to guess a letter, enter the answer, give up, or quit
 
